417cd922922a575da96f425e954cefb9ba1e45e0
Pénzes Ákos <penzesakos00@gmail.com>
2022-05-18 14:49:16 +0200

    Added dummy test to check if JaCoCo works correctly

---
+++ main/src/com/complexible/DummyClass.java
@@ -0,0 +1,12 @@
+package com.complexible;
+
+public class DummyClass {
+
+    public DummyClass(){
+
+    }
+
+    public static void foo(){
+        System.out.println("foo");
+    }
+}
---
+++ test/src/com/complexible/TestDummyClass.java
@@ -0,0 +1,11 @@
+package com.complexible;
+
+import org.junit.Test;
+
+public class TestDummyClass {
+    @Test
+    public void dummy_test(){
+        DummyClass dc = new DummyClass();
+        DummyClass.foo();
+    }
+}
